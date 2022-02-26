1cc6dd3720c72dd06c532085c56e2e5c2e2d3cdf
Abhishek sahu <98146826+150278@users.noreply.github.com>
2022-02-26 21:52:04 +0530

    Update way 1.sh

--- way 1.sh
+++ way 1.sh
@@ -17,8 +17,8 @@ List out the output of all mp3 files of system and play as playlist
 "
 
 
-:
-"
+
+
 #!/bin/bash
 #echo $""
 
@@ -72,24 +72,7 @@ esac
 done
 
 
-"
 
-#!/bin/bash
-
-
-find  -type f -name "*.mp3" > playlist.txt
-cat playlist.txt
-inp=1
-
-while [ $inp == 1  ]
-do
-echo " Enter song name from list "
-read ENTER
-play "$ENTER.mp3"
-inp=0
-echo "Enter 1 to play or 0 to exit"
-read inp
-done
 
 
 
